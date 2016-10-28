b30508fce62b58acc18c9f6395306c1ec058e0e3
cyper <345343747@qq.com>
2016-10-28 17:27:38 +0800

    same with simple table demo

--- FoodPinDemo/RestaurantTableViewController.swift
+++ FoodPinDemo/RestaurantTableViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class RestaurantTableViewController: UITableViewController {
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,23 +30,25 @@ class RestaurantTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return restaurantNames.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         // Configure the cell...
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = #imageLiteral(resourceName: "restaurant")
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
