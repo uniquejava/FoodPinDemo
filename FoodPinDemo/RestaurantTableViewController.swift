54f6e97641dfefa742891a9ff2be8a96231b1d6c
cyper <345343747@qq.com>
2016-10-28 19:52:45 +0800

    rounded corner image.

--- FoodPinDemo/RestaurantTableViewController.swift
+++ FoodPinDemo/RestaurantTableViewController.swift
@@ -47,7 +47,9 @@ class RestaurantTableViewController: UITableViewController {
         // Configure the cell...
         cell.nameLabel.text = restaurantNames[indexPath.row]
         cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
-
+        //cell.thumbnailImageView.layer.cornerRadius = 30.0
+        //cell.thumbnailImageView.clipsToBounds = true
+        
         return cell
     }
     
