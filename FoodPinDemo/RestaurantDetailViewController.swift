2639ac47336103438bed2980afd1115d7757e9ff
cyper <345343747@qq.com>
2016-10-29 02:40:55 +0800

    complete the plain detail view.

--- FoodPinDemo/RestaurantDetailViewController.swift
+++ FoodPinDemo/RestaurantDetailViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class RestaurantDetailViewController: UIViewController {
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var typeLabel: UILabel!
+class RestaurantDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    //@IBOutlet weak var nameLabel: UILabel!
+    //@IBOutlet weak var locationLabel: UILabel!
+    //@IBOutlet weak var typeLabel: UILabel!
     @IBOutlet weak var restaurantImageView: UIImageView!
     var restaurant: Restaurant!
     
@@ -29,6 +29,33 @@ class RestaurantDetailViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RestaurantDetailTableViewCell
+        
+        switch indexPath.row {
+        case 0:
+            cell.fieldLabel.text = "Name"
+            cell.valueLabel.text = restaurant.name
+        case 1:
+            cell.fieldLabel.text = "Type"
+            cell.valueLabel.text = restaurant.type
+        case 2:
+            cell.fieldLabel.text = "Location"
+            cell.valueLabel.text = restaurant.location
+        case 3:
+            cell.fieldLabel.text = "Been here"
+            cell.valueLabel.text = restaurant.isVisited ? "Yes, I've been here" : "No"
+        default:
+            cell.fieldLabel.text = ""
+            cell.valueLabel.text = ""
+        }
+        return cell
     }
     
 
