0c87990d69657c6e76f0d9acf72c17c6bd64aac6
cyper <345343747@qq.com>
2016-10-31 01:14:26 +0800

    present UIImagePickerController from didSelectRowAt

---
+++ FoodPinDemo/AddRestaurantController.swift
@@ -0,0 +1,40 @@
+//
+//  AddRestaurantController.swift
+//  FoodPinDemo
+//
+//  Created by cyper on 31/10/2016.
+//  Copyright © 2016 cyper tech. All rights reserved.
+//
+
+import UIKit
+
+class AddRestaurantController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.allowsEditing = false
+                imagePicker.sourceType = .photoLibrary
+                
+                present(imagePicker, animated: true, completion: nil)
+            }
+        }
+    }
+
+}
