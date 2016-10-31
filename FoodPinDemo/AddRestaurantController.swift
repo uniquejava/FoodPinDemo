c65683f8cb4c5636aac999d82bc75939cfae68b8
cyper <345343747@qq.com>
2016-10-31 14:17:30 +0800

    NSLayoutConstraint

--- FoodPinDemo/AddRestaurantController.swift
+++ FoodPinDemo/AddRestaurantController.swift
@@ -48,6 +48,21 @@ class AddRestaurantController: UITableViewController, UIImagePickerControllerDel
             photoImageView.clipsToBounds = true
         }
         
+        // Add 4 constraints
+        let superview = photoImageView.superview
+        
+        let leading = NSLayoutConstraint(item: photoImageView, attribute: .leading, relatedBy: .equal, toItem: superview, attribute: .leading, multiplier: 1, constant: 0)
+        leading.isActive = true
+        
+        let trailing = NSLayoutConstraint(item: photoImageView, attribute: .trailing, relatedBy: .equal, toItem: superview, attribute: .trailing, multiplier: 1, constant: 0)
+        trailing.isActive = true
+        
+        let top = NSLayoutConstraint(item: photoImageView, attribute: .top, relatedBy: .equal, toItem: superview, attribute: .top, multiplier: 1, constant: 0)
+        top.isActive = true
+        
+        let bottom = NSLayoutConstraint(item: photoImageView, attribute: .bottom, relatedBy: .equal, toItem: superview, attribute: .bottom, multiplier: 1, constant: 0)
+        bottom.isActive = true
+        
         dismiss(animated: true, completion: nil)
     }
 
