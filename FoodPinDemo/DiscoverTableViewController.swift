d29e52d01641ace5c36c2dea50d8228b5f877bec
cyper <345343747@qq.com>
2016-11-03 17:30:27 +0800

    cache image fileURL in NSCache

--- FoodPinDemo/DiscoverTableViewController.swift
+++ FoodPinDemo/DiscoverTableViewController.swift
@@ -13,6 +13,7 @@ class DiscoverTableViewController: UITableViewController {
     @IBOutlet var spinner: UIActivityIndicatorView!
     
     var restaurants: [CKRecord] = []
+    var imageCache = NSCache<CKRecordID, NSURL>()
     
     
     override func viewDidLoad() {
@@ -80,24 +81,43 @@ class DiscoverTableViewController: UITableViewController {
         cell.textLabel?.text = r.object(forKey: "name") as? String
         cell.imageView?.image = #imageLiteral(resourceName: "photoalbum")
         
-        let publicDb = CKContainer.default().publicCloudDatabase
-        let fetchOp = CKFetchRecordsOperation(recordIDs: [r.recordID])
-        fetchOp.desiredKeys = ["image"]
-        fetchOp.queuePriority = .veryHigh
-        fetchOp.perRecordCompletionBlock = {record, recordID, error in
-            if let error = error {
-                print("failed to get image from iCould: \(error.localizedDescription)")
-                return
+        // check if image stored in cache
+        if let imageFileURL = imageCache.object(forKey: r.recordID) {
+            // fetch image from cache
+            print("get image from cache")
+            if let imageData = try? Data.init(contentsOf: imageFileURL as URL) {
+                cell.imageView?.image = UIImage(data: imageData)
             }
+        } else {
+            // fetch image from cloud in background
             
-            if let rRecord = record {
-                OperationQueue.main.addOperation {
-                    self.fillCell4Image(cell, with: rRecord)
+            let publicDb = CKContainer.default().publicCloudDatabase
+            let fetchOp = CKFetchRecordsOperation(recordIDs: [r.recordID])
+            fetchOp.desiredKeys = ["image"]
+            fetchOp.queuePriority = .veryHigh
+            fetchOp.perRecordCompletionBlock = {record, recordID, error in
+                if let error = error {
+                    print("failed to get image from iCould: \(error.localizedDescription)")
+                    return
                 }
+                
+                if let rRecord = record {
+                    OperationQueue.main.addOperation {
+                        if let image = rRecord.object(forKey: "image") {
+                            let imageAsset = image as! CKAsset
+                            if let imageData = try? Data.init(contentsOf: imageAsset.fileURL) {
+                                cell.imageView?.image = UIImage(data: imageData)
+                            }
+                            // add the image URL to cache
+                            self.imageCache.setObject(imageAsset.fileURL as NSURL, forKey: r.recordID)
+                        }
+                    }
+                }
+                
             }
-
+            publicDb.add(fetchOp)
         }
-        publicDb.add(fetchOp)
+
         
         // fillCell(cell, with: r)
         
